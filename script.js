eeb86e7d1925d1dfe74b10262e8502cdaa9bdcdc
lakshayvaid <117804299+lakshayvaid@users.noreply.github.com>
2023-05-20 14:52:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let f=document.getElementById("foods");
+
+console.log(f);
+
+
+f.onclick(()=>{
+
+document.g.innerHTML = "<select id ="fooditems">  
+<option> apple </option>  
+<option>banana</option>  
+<option> mango </option>  
+<option> orange</option>  
+<option> guava </option>  
+</select>";
+
+})
